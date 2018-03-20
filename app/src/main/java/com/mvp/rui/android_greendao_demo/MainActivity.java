8e90e85921f5ed479e6431b2ab6db922b192ed45
liurui <1513093228@qq.com>
2018-03-20 16:25:20 +0800

    添加一点注释，测试上传代码

--- app/src/main/java/com/mvp/rui/android_greendao_demo/MainActivity.java
+++ app/src/main/java/com/mvp/rui/android_greendao_demo/MainActivity.java
@@ -3,6 +3,9 @@ package com.mvp.rui.android_greendao_demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 一个greendao的框架项目
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
