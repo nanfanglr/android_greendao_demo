3c9cfb24c0f19b879ae52fc1101bdb2f1ccb5b8e
liurui <1513093228@qq.com>
2018-03-20 16:38:54 +0800

    添加greendao依赖

---
+++ app/src/main/java/com/mvp/rui/android_greendao_demo/MyApplication.java
@@ -0,0 +1,12 @@
+package com.mvp.rui.android_greendao_demo;
+
+import android.app.Application;
+
+/**
+ * Created by 0200030 on 2018/3/20.
+ */
+
+public class MyApplication extends Application {
+
+    
+}
